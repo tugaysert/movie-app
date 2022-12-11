6ea7eafc88f41aaa82c95dc974e7e83b405f43c0
tugaysert <101674140+tugaysert@users.noreply.github.com>
2022-12-11 23:13:33 +0300

    -

--- movie-app-backend/src/main/java/com/example/movieappbackend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.movieappbackend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- movie-app-backend/src/main/java/com/example/movieappbackend/repository/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.movieappbackend.repository;
-
-import com.example.movieappbackend.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> { }
